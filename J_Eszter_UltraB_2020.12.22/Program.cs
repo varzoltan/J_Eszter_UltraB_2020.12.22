29d208012e8620a7c2ba4748083227300df305dc
varzoltan <64203391+varzoltan@users.noreply.github.com>
2020-12-22 12:06:02 +0100

    12:05

--- J_Eszter_UltraB_2020.12.22/Program.cs
+++ J_Eszter_UltraB_2020.12.22/Program.cs
@@ -8,6 +8,14 @@ namespace J_Eszter_UltraB_2020._12._22
 {
     class Program
     {
+        struct Adat
+        {
+            public string nev;
+            public int rajtszam;
+            public string kategoria;
+            public string ido;
+            public int tavszazalek;
+        }
         static void Main(string[] args)
         {
 
