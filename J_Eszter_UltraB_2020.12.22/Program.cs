fe974fc9a6a3536830748d311433cfc196a0510f
varzoltan <64203391+varzoltan@users.noreply.github.com>
2020-12-22 11:56:01 +0100

    Add project files.

---
+++ J_Eszter_UltraB_2020.12.22/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J_Eszter_UltraB_2020._12._22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
