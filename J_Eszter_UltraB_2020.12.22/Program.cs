9277a3d2c856cc21b9a60d68eeed0c6d965133fe
varzoltan <64203391+varzoltan@users.noreply.github.com>
2020-12-29 15:02:16 +0100

    15:02

--- J_Eszter_UltraB_2020.12.22/Program.cs
+++ J_Eszter_UltraB_2020.12.22/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace J_Eszter_UltraB_2020._12._22
 {
@@ -18,7 +19,29 @@ namespace J_Eszter_UltraB_2020._12._22
         }
         static void Main(string[] args)
         {
+            Adat[] adatok = new Adat[500];
+            
+            StreamReader beolvas = new StreamReader(@"E:\OneDrive - Kisvárdai SZC Móricz Zsigmond Szakgimnáziuma és Szakközépiskolája\Oktatas\Programozas\Jakab_Acs_Eszter\Erettsegi_feladatok\2019_maj_Ultrabalaton\ub2017egyeni.txt");
+            string nemKell = beolvas.ReadLine();
+            int n = 0;
+            while (!beolvas.EndOfStream)
+            {
+                string sor = beolvas.ReadLine();
+                string[] db = sor.Split(';');
+                adatok[n].nev = db[0];
+                adatok[n].rajtszam = int.Parse(db[1]);
+                adatok[n].kategoria = db[2];
+                adatok[n].ido = db[3];
+                adatok[n].tavszazalek = int.Parse(db[4]);
+                n++;
+            }
+            beolvas.Close();
+            Console.WriteLine($"2. feladat: Beolvasás kész!");
 
+            //3.feladat
+            Console.WriteLine($"3. feladat: Egyéni indulók: {n} fő");
+
+            Console.ReadKey();
         }
     }
 }
